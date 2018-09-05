70ea5b4532054464ac022c932165efb5299d8776
norah <norahaaltwijri@gmail.com>
2018-09-05 21:36:38 +0300

    Initial commit

---
+++ header.php
@@ -0,0 +1,41 @@
+<html class="no-js" lang="en">
+  
+<!-- Mirrored from pixelosaur.com/preview/altius/theme/index-fullscreen-image.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 24 Nov 2016 10:38:09 GMT -->
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Meta Tags -->
+    <meta name="description" content="????? ????????? ???????? ???????????" />
+    <meta name="author" content="raedoon" />
+    <title>?????? | ???? ???? ????? ??? ?????</title>
+
+    <!-- Favicons:: Replace with yours -->
+    <link rel="shortcut icon" href="img/favicons/favicon-32x32.png">
+
+    <!-- Stylesheets and Fonts-->
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:300,400,400italic,700|Oxygen:400,300,700' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="css/foundation.min.css" />
+    <link rel="stylesheet" href="css/main.min.css" />
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/animsition.min.css">
+    <link rel="stylesheet" href="css/icomoon.css" />
+    <link rel="stylesheet" href="css/pogo-slider.min.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" id="default" href="css/colors/colors.css" />
+    <link rel="stylesheet" href="css/rtl.css">
+    <link rel="stylesheet" href="css/custom.css">
+    <link rel="stylesheet" href="css/normalize.css" />
+    <link rel="stylesheet" href="css/component.css" />
+    <link rel="stylesheet" href="css/set2.css" />
+
+    <!-- Scripts -->
+    <script src="js/vendor/jquery.min.js"></script>
+    <!--[if IE]>
+      <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <!--[if lt IE 9]>
+      <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+    <![endif]-->
+  </head>
